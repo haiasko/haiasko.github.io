cda855c3bba42b4901fd16c60cb1a2d863b98b28
Plamen Nikolaev <plamen@gmail.com>
2014-03-12 05:16:01 +0100

    and bleh, no symlinks in _includes

--- _includes/Alert.hs
+++ _includes/Alert.hs
@@ -1,1 +1,11 @@
-../Alert.hs
+module Alert where
+
+import           FFI
+import           Prelude
+
+main :: Fay ()
+main = alert "Hello, World!"
+
+-- | Alert using window.alert.
+alert :: String -> Fay ()
+alert = ffi "window.alert(%1)"
